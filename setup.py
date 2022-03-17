1735c0b894f9db911d2adf8e9574b2174c8b31f3
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-03-17 09:32:52 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='pyBibX',
+    version='1.1.4',
+    license='GNU',
+    author='Valdecy Pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pyBibX',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'networkx',
+        'numpy',
+        'pandas',
+        'plotly',
+        'squarify',
+        'matplotlib',
+        'scipy',
+        'sklearn',
+        'wordcloud'
+    ],
+    zip_safe=True,
+    description='A Bibliometric and Scientometric Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
