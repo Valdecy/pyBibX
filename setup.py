ebe0e731e21e6574f36248266742334465756d62
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-04-07 12:20:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyBibX',
-    version='1.7.0',
+    version='1.7.3',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
@@ -31,10 +31,11 @@ setup(
         'torchaudio',
         'transformers',
         'umap-learn',
+        'openai',
         'wordcloud'
     ],
     zip_safe=True,
-    description='A Bibliometric and Scientometric Library',
+    description='A Bibliometric and Scientometric Library Powered with Artificial Intelligence Tools',
     long_description=long_description,
     long_description_content_type='text/markdown',
 )
