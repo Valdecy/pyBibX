64b631f5062ebb606598ee9ec0f70bcf8c885f6a
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-06-15 21:09:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyBibX',
-    version='3.0.5',
+    version='3.0.7',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
